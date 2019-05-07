c6f5d9136dac5c04e2d41189fec53122433420ed
glory2hypnotoad <matthew.limbach1@icloud.com>
2019-05-07 13:17:35 -0400

    index handlebar created

--- db/schema.sql
+++ db/schema.sql
@@ -7,7 +7,7 @@ USE burgers_db;
 CREATE TABLE burgers (
     id INTEGER NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR (100),
-    devoured BOOLEAN,
+    devoured BOOLEAN DEFAULT false,
     PRIMARY KEY (id)
 
 );
