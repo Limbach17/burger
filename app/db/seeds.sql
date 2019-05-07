f6437a1a1c3ac16a477a684eb79fabc89d33cbc4
glory2hypnotoad <matthew.limbach1@icloud.com>
2019-05-06 23:51:07 -0400

    app initilized with Database, PORT

---
+++ app/db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Good burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Pittsburgher", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Nothingburger", false);
